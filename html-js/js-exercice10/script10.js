ad6420fe6f4fb8205607c84882f4e0d4b6ec3d4b
amineshwarz <elkhal.medamine@gmail.com>
2025-06-30 14:48:44 +0200

    push de 30/06 fin de journée

---
+++ html-js/js-exercice10/script10.js
@@ -0,0 +1,57 @@
+// Exercice 10-1 (JS - Cacher un élément)
+        // let cacher = document.getElementById('deuxieme');
+        // cacher.classList.add("hidden");
+
+// Exercice 10-2 (JS - Spoiler)
+// document.getElementById('btn').onclick = function antiSpoile() {
+//     let cacher = document.getElementsByClassName('spoil');
+//     for (let i = 0; i < cacher.length; i++) {
+//         cacher[i].classList.add("hidden");
+//     }
+// }
+
+// Exercice 10-3 (JS - Surbillance au clic)
+
+ 
+let test = document.getElementsByTagName("p"); 
+
+for (let i = 0; i < test.length; i++) {
+    test[i].onclick = function surbillance(e) { 
+        e.target.style.backgroundColor = e.target.style.backgroundColor === "yellow" ? "white" : "yellow";
+        // if (test[i].style.backgroundColor === "yellow") {
+        //     test[i].style.backgroundColor = "white";
+        // } else {
+        //     test[i].style.backgroundColor = "yellow";
+        // } 
+    }
+}
+
+function aa() { // on appui sur le boutton et tout les paragraphes deviennent jaune
+    let pp = document.getElementsByTagName("p");
+    for (let i = 0; i < pp.length; i++) {
+        pp[i].style.backgroundColor = "yellow"; 
+    }
+
+}
+
+
+
+
+// Exercice 10-4 (JS - Fitrage par catégorie)
+function filtreItems() {
+    let items = document.querySelector("item");
+    let categorie = "fruit";
+    
+    for (let i = 0; i < items.length; i++) {
+        if (items[i].classList.contains(categorie)) {
+            items[i].style.display = "block"; // Affiche l'élément
+        }
+        else {
+            items[i].style.display = "none"; // Cache l'élément
+        }
+    }
+    console.log(items);
+    console.log(categorie);
+
+}
+
