65881a347e1bfc634891d8ca49724e5be2bcd8aa
amineshwarz <elkhal.medamine@gmail.com>
2025-07-08 15:42:41 +0200

    exercice12 07/08 fin de journée

--- html-js/js-exercice12/script12.js
+++ html-js/js-exercice12/script12.js
@@ -1,4 +1,4 @@
-// JS Exercice 12-1 Composant: Accordéon 
+// ----------------JS Exercice 12-1 Composant: Accordéon ---------------------------------
 
 // Sélectionner tous les éléments avec la classe 'accordion-title'
 const allTitle = document.querySelectorAll('.accordion-title');
@@ -13,22 +13,27 @@ allTitle.forEach(function(title) {
     if (content.classList.contains('show')) {
       content.classList.remove('show');
     } else {
+      // Cacher tous les autres contenus
+      const allContent = document.querySelectorAll('.accordion-content');
+      allContent.forEach(function(item) {
+        item.classList.remove('show');
+      });
       content.classList.add('show');
     }
   };
 });
 
-// JS Exercice 12-2 Composant: galerie video avec des boutons à l'aide d'iframe et innerHTML 
+// -----------------------JS Exercice 12-2 Composant: galerie video avec des boutons à l'aide d'iframe et innerHTML 
 
 // Définir tableaux des vidéos à afficher
 const videos = [
   {
     title: 'Vidéo 1',
-    url: 'https://www.youtube.com/embed/daWr9xnkKS4'
+    url: 'https://www.youtube.com/embed/QusiVkxxpXI'
   },
   {
     title: 'Vidéo 2',
-    url: 'https://www.youtube.com/embed/CyWGhmTWHM8'
+    url: 'https://www.youtube.com/embed/QusiVkxxpXI'
   },
   {
     title: 'Vidéo 3',
@@ -47,7 +52,7 @@ function changeVideo(index) {
 
 // Créer des boutons pour chaque vidéo
 videos.forEach((video, index) => {
-  let button = document.createElement('button');
+  const button = document.createElement('button');
   button.textContent = video.title;
   //Lorsqu'on clique, la fonction 'changeVideo' est appelée avec l'index de la vidéo correspondante, ce qui met à jour la vidéo affichée.
   // button.onclick = function() {
@@ -56,9 +61,51 @@ videos.forEach((video, index) => {
   button.onclick = () => changeVideo(index);
   buttonContainer.appendChild(button);
 });
-
 // Afficher la première vidéo par défaut
 changeVideo(0);
+
+
+
+// ------------------------------ JS Exercice 12-3 Composant: Carrousel d'images avec des boutons----------
+
+let currentSlide = 0; // Index de la slide actuelle
+const pictures = [
+  './img/aminePirate.JPG',
+  './img/arnaudPirate.JPG',
+  './img/jeremiePirate.JPG' 
+];
+
+function changeSlide(index) {
+  // Calcule l'index de la nouvelle slide
+  currentSlide = (currentSlide + index + pictures.length) % pictures.length;
+
+  // Créer le code img avec innerHTML
+  const carouselContainer = document.getElementById('carousel-container');
+  carouselContainer.innerHTML = `<img src="${pictures[currentSlide]}" class="slide active">`;
+
+  // creation des boutton dans for
+  for (let i = 0; i < pictures.length; i++) {
+        // Créer des boutons pour naviger prev et next
+      const carouselButtons = document.getElementById('carousel-buttons');
+      const prevButton = document.createElement('button');
+      prevButton.type = "button";
+      prevButton.textContent = '<';
+      prevButton.onclick = () => changeSlide(-1);
+      
+    
+      const nextButton = document.createElement('button');
+      nextButton.type = "button";
+      nextButton.textContent = '>';
+      nextButton.onclick = () => changeSlide(1); 
+
+      carouselButtons.innerHTML = ''; // Clear previous buttons
+      carouselButtons.appendChild(prevButton);
+      carouselButtons.appendChild(nextButton);
+      
+  }
+}
+// Afficher la première image par défaut
+changeSlide(0);
 
 
 
